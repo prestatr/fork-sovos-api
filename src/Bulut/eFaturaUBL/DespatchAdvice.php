b17066ae3bdc4d6340b38607fa912260871aee5e
Ahmet Imamoglu <ahmet363@gmail.com>
2023-06-25 02:52:42 +0300

    DespatchAdvice için eksik değişkenler ilave edildi.

--- src/Bulut/eFaturaUBL/DespatchAdvice.php
+++ src/Bulut/eFaturaUBL/DespatchAdvice.php
@@ -1,52 +1,61 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: orhangazibasli
- * Date: 24.12.2017
- * Time: 14:22
- */
 
 namespace Bulut\eFaturaUBL;
 
-
 class DespatchAdvice
 {
+    public string $UBLVersionID;
+
+    public string $CustomizationID;
+
+    public string $ProfileID;
+
+    public string $ID;
+
+    public string $CopyIndicator;
+
+    public string $UUID;
+
+    public string $IssueDate;
+
+    public string $IssueTime;
+
+    public string $DespatchAdviceTypeCode;
+
+    public int $LineCountNumeric;
 
     /**
-     * @var |String
-     */
-    public $DespatchAdviceTypeCode;
-
-
-    /**
-     * @var |Bulut|eFaturaUBL|OrderReference
+     * @var \Bulut\eFaturaUBL\OrderReference
      */
     public $OrderReference;
 
     /**
-     * @var |Bulut|eFaturaUBL|AdditionalDocumentReference
+     * @var \Bulut\eFaturaUBL\AdditionalDocumentReference
      */
     public $AdditionalDocumentReference;
 
     /**
-     * @var |Bulut|eFaturaUBL|Signature
+     * @var \Bulut\eFaturaUBL\Signature
      */
     public $Signature;
 
     /**
-     * @var |Bulut|eFaturaUBL|DespatchSupplierParty
+     * @var \Bulut\eFaturaUBL\DespatchSupplierParty
      */
     public $DespatchSupplierParty;
 
     /**
-     * @var |Bulut|eFaturaUBL|DeliveryCustomerParty
+     * @var \Bulut\eFaturaUBL\DeliveryCustomerParty
      */
     public $DeliveryCustomerParty;
 
     /**
-     * @var |Bulut|eFaturaUBL|DespatchLine Array
+     * @var \Bulut\eFaturaUBL\Shipment
+     */
+    public $Shipment;
+
+    /**
+     * @var \Bulut\eFaturaUBL\DespatchLine Array
      */
     public $DespatchLine;
-
-
 }
